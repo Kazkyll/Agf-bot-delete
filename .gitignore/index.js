09c75184b1fa60259ddf0c42e16d8f1518c6cf0d
Kazkyll <37412917+Kazkyll@users.noreply.github.com>
2018-12-14 22:23:47 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -121,10 +121,13 @@ bot.on('message', function (message) {
 
 bot.on('message', function (message) {
   if (message.content === (prefix + 'help') ) {
-    message.channel.send('La commande *help ou  *HELP vous sert à connaitre les actions que Conchita peut faire. ')
-    message.channel.send('La commande *IOS ou *ios vous informe que la dernière version validé de Assassin Sniper 3D pour IOS. ')
-    message.channel.send('La commande *Android ou *android vous informe que la dernière version validé de Assassin Sniper 3D pour Android. ')
-    message.channel.send('La commande *AdmissionNew permet de remettre le texte du salon Admission après un nettoyage a n utiliser que dans le salon admission svp !!!. ')
+    message.channel.send('La commande *help vous sert à connaitre les actions que Conchita peut faire. ')
+    message.channel.send('La commande *ping vous sert à tester si Conchita fonctionne. ')
+    message.channel.send('La commande *ios vous informe que la dernière version validé de Assassin Sniper 3D pour IOS. ')
+    message.channel.send('La commande *android vous informe que la dernière version validé de Assassin Sniper 3D pour Android. ')
+    message.channel.send('La commande *supchat vous sert à effacer completement un salon (100 message à la fois seulemnt). **ATTENTION** ')
+    message.channel.send('La commande *AdmissionNew permet de remettre le texte du salon Admission après un nettoyage à n’utiliser **que dans le salon admission svp !!!**.  ')
+
   }
 });
 
