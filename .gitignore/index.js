781c5ea77a955f77de2615d84c98c9cde6a4135f
Kazkyll <37412917+Kazkyll@users.noreply.github.com>
2018-12-29 18:14:10 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -73,16 +73,26 @@ bot.on('message', msg => {
 //  parametre des version validé des OS du jeu
 
 bot.on('message', function (message) {
+  if (message.content === prefixA + '01') {
+    message.reply('Le testeur officiel AGF vous informe que la dernière version validée de Assassin Sniper 3D pour IOS est la 2.21.3')
+  }
+});
+
+bot.on('message', function (message) {
   if (message.content === prefixA + '02') {
     message.reply('Le testeur officiel AGF vous informe que la dernière version validée de Assassin Sniper 3D pour Android est la 2.16.19')
   }
 });
 
+//----------------------------------------------------------------------------------------------------------------------
+//  parametre des version non validé des OS du jeu
+
 bot.on('message', function (message) {
-  if (message.content === prefixA + '01') {
-    message.reply('Le testeur officiel AGF vous informe que la dernière version validée de Assassin Sniper 3D pour IOS est la 2.21.3')
+  if (message.content === prefixA + '03') {
+    message.reply('Le testeur officiel AGF vous informe que les versions suivante pour Android: 2.16.16 - 2.16.17 - 2.16.18 sont non validée et comportent des bugs')
   }
 });
+
 
 //---------------------------------------------------------------------------------------------------------------------------
 //Script de suppression des messages
@@ -125,7 +135,7 @@ bot.on("message", msg => {
 //------------------------------------------------------------------------------------------------------
 // Message de l'admission
 bot.on('message', function (message) {
-  if (message.content === prefixA + '03') {
+  if (message.content === prefixA + '04') {
     var admission = bot.channels.get("509473089277657100")
     admission.send('\n\n\n')
     admission.send('\n\n\n-**Bienvenue chez Ghost French !**-\n\n\nVous vous trouvez dans un salon d’accueil destiné à l’intégration des nouveaux venus.\nSi vous êtes un visiteur d’une autre escouade venu faire connaissance, déplacez-vous dans le salon Place publique,\nqui vous est destiné, et laissez un "bonjour", ce qui nous permettra de remarquer votre présence et venir papoter avec vous.\nSi vous êtes postulant ou déjà membre de AGF ★ A Ghost French, vous êtes au bon endroit.\nPour le moment vous ne pouvez lire et écrire des messages qu’ici, mais dès que vous aurez signalé votre présence par un "bonjour" ou même une courte présentation, un officier de l’escouade vous donnera le statut de Rookie et vous pourrez explorer les autres salons et discuter avec tout le monde.\nLa langue parlée ici est le français mais nous avons quelques anglophones.\nS’il vous plait, faites en sorte que votre PSEUDO ici corresponde à celui que vous utilisez sur le JEU.\nConseil : désactivez les notifications de Discord sur votre appareil si vous ne voulez pas qu’elles viennent vous déranger en plein match PVP.\nAttention : tous les messages déposés dans ce salon sont destinés à être supprimés dès que l’admission sera terminée.\n\n\n----------------------------------------------\nAGF**AGF**AGF**AGF**AGF**AGF**AGF**AGF**AGF\n----------------------------------------------\n\n\n-**Welcome to Ghost French!**-\n\n\nYou are in a reception lounge for the integration of newcomers.\nIf you are a visitor from another squad, move to the Place publique chatroom, which is dedicated to you, and leave a "hello",\nwhich will allow us to notice your presence and come for a chat with you.\nIf you are an applicant or already member of AGF ★ A Ghost French,you are in the right place..\nAt the moment you can read and write messages only here but as soon as you have reported your presence by a "hello" or even a short presentation if you are a candidate,')
